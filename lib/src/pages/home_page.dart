1c3d4bed26fe23830ddd3f4567615b16fa2697c8
ewertonl <ewertonlsantos@protonmail.com>
2022-08-12 23:48:04 -0300

    feat(widget): create home page layout blocking

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -10,38 +10,89 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        child: CustomScrollView(
+          physics: const BouncingScrollPhysics(),
+          slivers: [
+            SliverAppBar(
+              // toolbarHeight: 80,
+              stretch: true,
+              title: Column(
+                children: const [
+                  Text(
+                    "Location",
+                    style: TextStyle(
+                      fontSize: 12,
+                    ),
+                  ),
+                  Text(
+                    "Cameron St., Boston",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ],
+              ),
+              centerTitle: true,
+              floating: true,
+              // pinned: true,
+              leadingWidth: 80,
+              // TODO: Drawer menu
+              leading: AspectRatio(aspectRatio: 1, child: const Placeholder()),
+              actions: const [
+                Placeholder(
+                  child: AspectRatio(
+                    aspectRatio: 1,
+                    child: SizedBox.expand(),
+                  ),
+                )
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            SliverPadding(
+              padding: const EdgeInsets.symmetric(vertical: 32),
+              sliver: SliverToBoxAdapter(
+                child: SizedBox(
+                  height: 50,
+                  // TODO: Pets list filter
+                  child: ListView(
+                    padding: const EdgeInsets.symmetric(horizontal: 32),
+                    physics: const BouncingScrollPhysics(),
+                    clipBehavior: Clip.none,
+                    scrollDirection: Axis.horizontal,
+                    children: List.generate(
+                      6,
+                      (index) {
+                        return Padding(
+                          padding: EdgeInsets.only(left: index == 0 ? 0 : 16),
+                          child: const Placeholder(
+                            child: SizedBox(width: 60, height: 60),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ),
             ),
+            SliverPadding(
+              padding: const EdgeInsets.symmetric(horizontal: 32),
+              sliver: SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                childCount: 10,
+                (context, index) {
+                  const double padding = 12;
+                  return Padding(
+                    padding: EdgeInsets.only(
+                      top: index == 0 ? 0 : padding,
+                    ),
+                    child: Placeholder(child: SizedBox(height: 120)),
+                  );
+                },
+              )),
+            )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
