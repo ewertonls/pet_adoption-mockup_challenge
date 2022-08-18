c4faa826f123d0db653f9093b972f7ed0d365a6a
ewertonl <ewertonlsantos@protonmail.com>
2022-08-17 23:02:44 -0300

    refactor: change button padding

--- lib/src/common_widgets/app_drawer_button.dart
+++ lib/src/common_widgets/app_drawer_button.dart
@@ -8,7 +8,7 @@ class AppDrawerButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(left: 16),
+      padding: const EdgeInsets.only(left: 20),
       child: LimitedBox(
         maxWidth: 50,
         child: AppIconButton(
